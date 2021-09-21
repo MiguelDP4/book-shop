2d98b9ea08fb88610fb7ce3161564be06ad52a13
MiguelDP4 <migueldp4@gmail.com>
2021-09-20 18:50:19 -0600

    add fields to signup form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) {
+      |u| u.permit(:name,
+                   :address,
+                   :phone,
+                   :email,
+                   :user_type,
+                   :password)
+      }
+    devise_parameter_sanitizer.permit(:account_update) {
+      |u| u.permit(:name,
+                   :address,
+                   :phone,
+                   :email,
+                   :password,
+                   :current_password)
+      }
+  end
 end
