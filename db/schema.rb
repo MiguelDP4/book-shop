4c69009a1da19922cfdc2fc33610b3d38cf05cb2
MiguelDP4 <migueldp4@gmail.com>
2021-09-20 19:54:27 -0600

    Add balance to user table, buyer starts with $50

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_09_21_000227) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "user_type", default: "", null: false
+    t.decimal "balance", default: "0.0", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
