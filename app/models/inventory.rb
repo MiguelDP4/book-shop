5834f73ef9b3e15898f332973ce53b9a593417fb
MiguelDP4 <migueldp4@gmail.com>
2021-09-22 20:31:13 -0600

    add validations for books and inventories

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,4 +1,8 @@
 class Inventory < ApplicationRecord
+  validates :user_id, presence: true
+  validates :sale_price, presence: true
+  validates :amount, presence: true
+  validates :book_id, presence: true
   belongs_to :user, optional: true
   belongs_to :sale, optional: true
   belongs_to :book
