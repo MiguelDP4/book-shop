9eff92b696e143bd2b5d7005b1fa2fbeea5e7283
MiguelDP4 <migueldp4@gmail.com>
2021-09-23 20:22:41 -0600

    can search books by seller, author, description and title

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,24 @@
 class HomeController < ApplicationController
   def index
-    @inventories = Inventory.all
+    search_term = params[:search_term]
+    search_type = params[:search_type]
+    if search_term
+      case search_type
+        when 'title'
+          @inventories = Inventory.joins(:book)
+          .where('lower(books.title) LIKE ?', "%#{search_term.downcase}%")
+        when 'description'
+          @inventories = Inventory.joins(:book)
+          .where('lower(books.description) LIKE ?', "%#{search_term.downcase}%")
+        when 'author'
+          @inventories = Inventory.joins(:book)
+          .where('lower(books.author) LIKE ?', "%#{search_term.downcase}%")
+        when 'seller'
+          @inventories = Inventory.joins(:user)
+          .where('lower(users.name) LIKE ?', "%#{search_term.downcase}%")
+      end
+    else
+      @inventories = Inventory.all
+    end
   end
 end
